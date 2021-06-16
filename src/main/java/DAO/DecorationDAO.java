8559f1d3a31e3b742a0a823126ab8bcc6f9f758d
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-06-16 12:34:13 -0500

    Implementing Decoration DAO

--- src/main/java/DAO/DecorationDAO.java
+++ src/main/java/DAO/DecorationDAO.java
@@ -1,4 +1,13 @@
 package DAO;
 
+import Model.Decoration;
+
 public interface DecorationDAO {
+
+    public Decoration getDecorationFromId(Long decoId);
+
+    public Decoration getDecorationFromDecoName(String decoName);
+
+    public Decoration getDecorationFromSkillName(String skillName);
+
 }
--- src/main/java/DAO/JDBCDecorationDAO.java
+++ src/main/java/DAO/JDBCDecorationDAO.java
@@ -1,7 +1,50 @@
 package DAO;
 
+import Model.Decoration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
-public class JDBCDecorationDAO {
+import javax.sql.DataSource;
 
+public class JDBCDecorationDAO implements DecorationDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JDBCDecorationDAO(DataSource ds) {
+        this.jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    @Override
+    public Decoration getDecorationFromId(Long decoId) {
+        String sql = "SELECT * FROM decoration WHERE deco_id = ?;";
+        SqlRowSet result = this.jdbcTemplate.queryForRowSet(sql,decoId);
+        if (result.next()) {
+            return mapRowToDecoration(result);
+        }
+        return null;
+    }
+
+    @Override
+    public Decoration getDecorationFromDecoName(String decoName) {
+        String sql = "SELECT * FROM decoration WHERE deco_name LIKE ?;";
+        SqlRowSet result = this.jdbcTemplate.queryForRowSet(sql,"%" + decoName + "%");
+        if (result.next()) {
+            return mapRowToDecoration(result);
+        }
+        return null;
+    }
+
+    @Override
+    public Decoration getDecorationFromSkillName(String skillName) {
+        String sql = "SELECT d.* FROM decoration d JOIN skill s " +
+                "ON d.skill_id = s.skill_id WHERE s.skill_name = ?;";
+        SqlRowSet result = this.jdbcTemplate.queryForRowSet(sql,skillName);
+        if (result.next()) {
+            return mapRowToDecoration(result);
+        }
+        return null;
+    }
+
+    private Decoration mapRowToDecoration(SqlRowSet row) {
+        return new Decoration(row.getLong("deco_id"),row.getLong("skill_id"),row.getString("deco_name"),row.getInt("slot_level"));
+    }
 }
