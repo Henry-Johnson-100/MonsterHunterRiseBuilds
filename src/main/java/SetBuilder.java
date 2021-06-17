1a2f3440373b2e31a02d5b3781f0605e8662f7a1
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-06-17 08:23:45 -0500

    Update SetBuilder.java
    changed pieces to a hashmap, Some minor fixes to initialize the skill list and remove skills if they have reached their max level. and changed occupied pieces to return a hashset

--- src/main/java/SetBuilder.java
+++ src/main/java/SetBuilder.java
@@ -4,69 +4,105 @@ import DAO.JDBCArmorDAO;
 import DAO.SkillDAO;
 import Model.Armor;
 import Model.Skill;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SetBuilder {
-    private Armor head;
-    private Armor chest;
-    private Armor arms;
-    private Armor waist;
-    private Armor legs;
-    private final List<Skill> skillSearchList;
+    private Map<String, Armor> setMap;
+    private List<Skill> skillSearchList; //Needs to stay as a list because the order matters
     private final ArmorDAO armorDAO;
     private final SkillDAO skillDAO;
     private final DecorationDAO decoDAO;
 
 
     public SetBuilder(ArmorDAO armorDAO, SkillDAO skillDAO, DecorationDAO decoDAO, List<String> skillNames) {
-        this.head = null;
-        this.chest = null;
-        this.arms = null;
-        this.waist = null;
-        this.legs = null;
+        this.setMap = new HashMap<>();
         this.armorDAO = armorDAO;
         this.skillDAO = skillDAO;
         this.decoDAO = decoDAO;
         this.skillSearchList = getSkillSearchListFromNames(skillNames);
     }
 
+    private String getQuery() {
+        StringBuilder sql = new StringBuilder("SELECT oar.armor_id, ar.armor_name FROM optimal_armor(");
+        for (int i = 0; i < this.skillSearchList.size(); i++) {
+            if (i == this.skillSearchList.size() - 1) {
+                sql.append("?");
+                continue; //should break out of the loop anyways
+            }
+            sql.append("?,");
+        }
+        sql.append(") AS oar JOIN armor ar ON aor.armor_id = ar.armor_id WHERE oar.armor_piece_type NOT IN (");
+        String[] occupiedPieceTypes = (String[]) this.getOccupiedPieceTypes().toArray(); //this feels weird but I think it works
+        for (int i = 0; i < occupiedPieceTypes.length; i++) {
+            sql.append(occupiedPieceTypes[i]);
+            if (i < occupiedPieceTypes.length - 1) {
+                sql.append(",");
+            }
+        }
+        sql.append(");");
+        return sql.toString();
+    }
+
     private List<Skill> getSkillSearchListFromNames(List<String> skillNames) {
         List<Skill> tempSkillList = new ArrayList<>();
+        Skill tempSkill;
         for (String skillName : skillNames) {
-            tempSkillList.add(this.skillDAO.getSkillFromName(skillName));
+            tempSkill = this.skillDAO.getSkillFromName(skillName);
+            tempSkill.setSkillLevel(0); //initialize all search skill levels to zero
+            tempSkillList.add(tempSkill);
         }
         return tempSkillList;
     }
 
-    private List<String> getOccupiedPieceTypes() {
-        List<String> occupiedPieceTypes = new ArrayList<>();
-        for (Armor piece : new Armor[]{this.head, this.chest, this.arms, this.waist, this.legs}) { //TODO make this a hashset instead of doing this gross
-            if (piece != null) {
-                occupiedPieceTypes.add(piece.getPieceType());
-            }
+    private Set<String> getOccupiedPieceTypes() {
+        return new HashSet<>(this.setMap.keySet());
+    }
+
+    private boolean ifMaxLevelRemoveSkillFromSearch(Skill searchSkill) {
+        if (searchSkill.getSkillLevel() == searchSkill.getMaxSkillLevel()) {
+            this.skillSearchList.remove(searchSkill);
+            return true;
         }
-        return occupiedPieceTypes;
+        return false;
+    }
+
+    /**
+     * For the skills in the list skillSearchList, updates their levels with the armor's skills passed as an arg.
+     * Will remove a skill from skillSearchList if its maximum skill is reached or exceeded.
+     * @param armorSkillMap
+     */
+    private void updateSearchSkillLevels(Map<String, Skill> armorSkillMap) {
+        for (Skill searchSkill : this.skillSearchList) {
+            if (!armorSkillMap.containsKey(searchSkill.getSkillName())) {
+                continue;
+            }
+            searchSkill.setSkillLevel(searchSkill.getSkillLevel() + armorSkillMap.get(searchSkill.getSkillName()).getSkillLevel());
+            ifMaxLevelRemoveSkillFromSearch(searchSkill);
+        }
     }
 
     private void assignArmorPiece(Armor armorPiece) {
         switch (armorPiece.getPieceType()) {
-            case "HEAD" -> this.head = armorPiece;
-            case "CHEST" -> this.chest = armorPiece;
-            case "ARMS" -> this.arms = armorPiece;
-            case "WAIST" -> this.waist = armorPiece;
-            case "LEGS" -> this.legs = armorPiece;
+            case "HEAD" -> this.setMap.put("HEAD", armorPiece);
+            case "CHEST" -> this.setMap.put("CHEST", armorPiece);
+            case "ARMS" -> this.setMap.put("ARMS", armorPiece);
+            case "WAIST" -> this.setMap.put("WAIST", armorPiece);
+            case "LEGS" -> this.setMap.put("LEGS", armorPiece);
         }
     }
 
     private void searchForArmorPiece() {
         //TODO finish this method, and probably the stored procedures that go before it
+        String sql = getQuery();
         Long armorId = -1L;
         Armor newPiece = this.armorDAO.getArmorFromId(armorId);
         newPiece.setSkills(this.skillDAO.getSkillsFromArmorId(armorId));
         this.assignArmorPiece(newPiece);
+        updateSearchSkillLevels(newPiece.getSkills());
     }
+    //TODO work on stored procedures. Need to think about adding 'AND armor_piece_type NOT IN []' and 'AND ...' (skill level + current searchSkill level < max Skill Level) to fully optimize the searches
 
     public void generateArmorSet() { //TODO think about it if this is void or if it should return a set object
         while (this.getOccupiedPieceTypes().size() <= 5) {
